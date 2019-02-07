12270e228c80835da1c0d743524d7d268cd7bc16
Stefan Jenkins <Stefan.jenkins@gmail.com>
2019-02-07 04:31:12 -0500

    Add project files.

---
+++ DjangoTutorial/DjangoTutorial/urls.py
@@ -0,0 +1,21 @@
+"""
+Definition of urls for DjangoTutorial.
+"""
+
+from django.conf.urls import include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', DjangoTutorial.views.home, name='home'),
+    # url(r'^DjangoTutorial/', include('DjangoTutorial.DjangoTutorial.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+]
