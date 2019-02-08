cd5e372b19299f2f6069445373252e4f7431d4dd
Stefan Jenkins <Stefan.jenkins@gmail.com>
2019-02-08 04:46:59 -0500

    Completed stage 3

--- DjangoTutorial/polls/urls.py
+++ DjangoTutorial/polls/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path 
 
 from . import views
-
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
     # /polls/5
-    path('<int:question_id>', views.detail, name='detail'),
+    path('<int:question_id>/', views.detail, name='detail'),
     # /polls/5/results/
     path('<int:question_id>/results', views.results, name='results'),
     #polls/5/vote/
